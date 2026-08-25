f5e64058a7018f684f7fa37c63eb06cd56faae8c
Weerawat14087 <weerawat.pung@bumail.net>
2026-08-25 10:41:16 +0700

    add Lab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID :1690701980
+ * Name       :Weerawat
+ * Section    :129C
+ * No.        :N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
